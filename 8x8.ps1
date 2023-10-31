a0a80c102a5e7d878023687914435da7420640d8
chloebergen <chloelanebergen@gmail.com>
2023-10-31 17:39:37 -0400

    Separated WMI & Package, added checker.

--- 8x8.ps1
+++ 8x8.ps1
@@ -1,3 +1,22 @@
+## Remove old files + create installation directories.
+Remove-Item -Path "C:\MedicusIT\8x8\*.*" -Force -SilentlyContinue
+
+$testPath = "C:\MedicusIT"
+if (Test-Path $testPath -PathType Leaf){
+    Write-Host "MedicusIT directory already exists."
+} else 
+    {New-Item -Path "C:\MedicusIT" -ItemType Directory}
+
+$testPath2 = "C:\MedicusIT\8x8"
+if (Test-Path $testPath2 -PathType Leaf){
+    Write-Host "8x8 directory already exists."
+} else 
+    {New-Item -Path "C:\MedicusIT\8x8" -ItemType Directory}
+
+# Set .Net to TLS1.2 (3072)
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+## Process Killer
 $process8x8 = Get-Process -Name '*8x8*'
 if ($null -ne $process8x8) {
     Stop-Process -InputObject $process8x8 -Force
@@ -20,6 +39,43 @@ if ($true -eq $programList){
 }
 
 
+## Download 8x8 
+$installUrl= "https://vod-updates.8x8.com/ga/work-64-msi-v8.7.2-3.msi"
+$installPath = "C:\MedicusIT\8x8"
+$filename = "work-64-msi-v8.7.2-3.msi"
+
+if (Get-Command Invoke-WebRequest -ErrorAction SilentlyContinue) {
+    Invoke-WebRequest -Uri "$installUrl" -OutFile "$installPath\$filename"
+} else {
+    (New-Object Net.WebClient).DownloadFile($installUrl, "$installPath\$filename")
+}
+
+
+## Install 8x8
+$installer = "$installPath\$filename"
+Start-Process msiexec "/i $installer /qn /norestart"
+
+
+## Check if successful
+$test = Get-Package -Name '*8x8*'
+
+if($test -eq $true){
+    Write-Host "$programInstallName installed successfully."
+} else { 
+    Write-Error "$programInstallName failed to install."
+}
+
+
+
+
+
+
+
+
+
+###############################
+######### WMI Version #########
+###############################
 
 ## WMI Objects
 $programList = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -like 'Threatlocker'}
@@ -31,4 +87,15 @@ foreach ($program in $programList){
 }
 
 
+$check = get-wmiobject -class win32_product | Where-Object {$_.Name -like $programInstallName}
 
+if($check)
+  {
+    Write-Host "$programInstallName installed successfully."
+    exit 0
+  }
+else
+  { 
+    Write-Error "$programInstallName failed to install."
+    exit 1
+  }
