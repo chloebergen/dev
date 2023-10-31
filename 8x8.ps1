2a99505be386ab82a6b25b708d7bec22de03f522
chloebergen <chloelanebergen@gmail.com>
2023-10-31 14:28:50 -0400

    Process killing.

--- 8x8.ps1
+++ 8x8.ps1
@@ -1,3 +1,11 @@
+$process8x8 = Get-Process -Name '*8x8*'
+if ($null -ne $process8x8) {
+    Stop-Process -InputObject $process8x8 -Force
+    Get-Process | Where-Object {$_.HasExited}
+} else {
+    Write-Host "8x8 is not currently running, continuing.."}
+
+
 ## Package Manager 
 ## Make sure to use PS5.1, not PS Core 7 -- 7 doesn't seem to show msi/msu/Programs as Package Providers..?
 $providers = Get-PackageProvider | Select-Object Name
