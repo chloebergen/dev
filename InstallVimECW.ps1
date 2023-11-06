c8ce43cde9dbddfb23ac6afb5752ff5bba26041f
chloebergen <chloelanebergen@gmail.com>
2023-11-06 12:13:51 -0500

    v1.0, tested & working in VM.

--- InstallVimECW.ps1
+++ InstallVimECW.ps1
@@ -6,16 +6,19 @@ Start-Transcript -Path $transcriptPath -Append
 Remove-Item -Path "C:\MedicusIT\VimECW\*.*" -Force -ErrorAction SilentlyContinue
 
 $testPath = "C:\MedicusIT"
-if (Test-Path $testPath -PathType Container){
+if (Test-Path $testPath -PathType Container) {
     Write-Host "MedicusIT directory already exists."
 } else 
-    {New-Item -Path "C:\MedicusIT" -ItemType Directory}
+    {New-Item -Path "C:\MedicusIT" -ItemType Directory
+}
 
 $testPath2 = "C:\MedicusIT\VimECW"
-if (Test-Path $testPath2 -PathType Container){
+if (Test-Path $testPath2 -PathType Container) {
     Write-Host "VimECW directory already exists."
 } else 
-    {New-Item -Path "C:\MedicusIT\VimECW" -ItemType Directory}
+    {New-Item -Path "C:\MedicusIT\VimECW" -ItemType Directory
+}
+
 
 # Set .Net to TLS1.2 (3072)
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
@@ -34,13 +37,13 @@ if (Get-Command Invoke-WebRequest -ErrorAction SilentlyContinue) {
 
 ## Install MSI
 $installer = "$installPath\$filename"
-
-Start-Process "msiexec.exe" -ArgumentList "/i $installer /qn PRODUCT_KEY=unzBthe5Zs4g36b8uKLdzh RUN_ON_STARTUP=""true"" /L*V ""C:\vim-connect-msi-install.log"""
+$installLog = "C:\MedicusIT\VimECW\InstallLog.txt"
+Start-Process -Wait $env:systemroot\system32\msiexec.exe -ArgumentList "/i $installer PRODUCT_KEY=unzBthe5Zs4g36b8uKLdzh RUN_ON_STARTUP=true /LV ""$installLog"""
 
 ## Check if successfully installed
 $test = Get-Package -Name '*Vim*'
 
-if($test.Count -gt 0){
+if($test.Count -gt 0) {
     Write-Host "Vim Connect ECW has been installed successfully."
 } else { 
     Write-Error "Vim Connect ECW failed to install."
