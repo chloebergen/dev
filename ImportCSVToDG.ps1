12dff64e7797272f70311591fec241813d928121
chloebergen <chloelanebergen@gmail.com>
2023-12-29 16:09:08 -0500

    Added checks for membership and if UPN exists

--- ImportCSVToDG.ps1
+++ ImportCSVToDG.ps1
@@ -1,19 +1,24 @@
-Import-Csv “C:\CSV\conant+report.csv” | foreach {Add-DistributionGroupMember -Identity “CorporateAP” -Member $_.alias}
 
-----------------
-
+## Variables
 $distributionGroup = "CorporateAP"
 $csv = "C:\CSV\conant+report.csv"
 
-
-
-Import-CSV $csv -Header "UPN" | 
-	ForEach {
-	Add-DistributionGroupMember –Identity $distributionGroup -Member $_.UPN
-    Write-Host -f Green "Added User to Distribution List:"$_.UPN
+## Function to check if the UPNs provided exist in this tenant 
+function UserExists($upn) {
+    $user = Get-EXOMailbox -Identity $upn -ErrorAction SilentlyContinue
+    return $user -ne $null
 }
 
-
-----
-
-Get-DistributionGroupMember -Identity "CorporateAP" | Select-Object DisplayName, PrimarySmtpAddress
+#Get Existing Members of the Distribution List
+$checkMembers =  Get-DistributionGroupMember -Identity $distributionGroup -ResultSize Unlimited | Select -Expand PrimarySmtpAddress
+ 
+## Import CSV, iterate through each row checking if UPNs exist >> adding each UPN to the specified DG
+Import-CSV $csv -Header "UPN" | 
+ForEach-Object {
+$upn = $_.UPN
+    if ((UserExists $upn) -and ($DLMembers -contains $_.UPN)){
+        Write-Host -ForegroundColor Green "Added $upn to $distributionGroup"
+        Add-DistributionGroupMember -Identity $distributionGroup -Member $_.UPN
+    } else {
+        Write-Host -ForegroundColor Yellow "$upn is already a member, or does not exist."
+}}
